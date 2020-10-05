331f3cb57928204e0fca0a05842d00da1df81301
Logan <logan.lieou@protonmail.com>
2020-10-05 01:57:44 -0500

    Updates

---
+++ precache-manifest.7f943f7c7848692991a12d1108bb0c1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361b4d025acb1456eb30b6fcf61a30c2",
+    "url": "/Bap/index.html"
+  },
+  {
+    "revision": "66770da1932d4ccbcf4f",
+    "url": "/Bap/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7110554f3ca2e7625e9a",
+    "url": "/Bap/static/js/2.bd6ba126.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Bap/static/js/2.bd6ba126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66770da1932d4ccbcf4f",
+    "url": "/Bap/static/js/main.fb5340b7.chunk.js"
+  },
+  {
+    "revision": "a4d7e12a2d4d4993b158",
+    "url": "/Bap/static/js/runtime-main.07570105.js"
+  }
+]);
